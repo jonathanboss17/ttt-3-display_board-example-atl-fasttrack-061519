a4c2dac01d82363874db54f874816281c2cf43cb
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-15 18:12:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,54 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+ def display_board 
+  
+     row = [" ", "|", " ", "|", " "] #5 = row.length
+    
+
+    
+    for i in row do 
+      print i
+    end
+    
+    puts""
+    print "-----"
+    puts ""
+    
+    for i in row do 
+      print i
+    end
+    
+    puts ""
+    print "-----"
+    puts ""
+    
+    for i in row do 
+      print i
+    end
+    
+ end
+
+x = [" ", "|", " ", "|", " "] #5 = row.length
+
+
+for i in x do 
+
+  print i 
+  
+  retry if i == x.length - 1 
+    puts ""
+    print "-----"
+    puts ""
+    
+  end
+  
+end
+#why does this not work
+
+
+#adding method 
+
+# def add_two_nums(x, y)
+#   puts x + y
+# end
+
+# add_two_nums(5, 5)
